e1d320222342e22ade5d77ef779fcc7379f603ba
Freddy <freddy.canido12@gmail.com>
2021-08-21 14:31:47 -0400

    list fail

--- src/ListChallenges.hs
+++ src/ListChallenges.hs
@@ -12,3 +12,7 @@ isPalindrome x = if x == reverse x then True else False
 --   newList++[x!!y| y <- [0..length x], x!!y /= [newList !! z| z<- [0..length newList]]]
 --   return newList
 -- tried to make a new list of unique items that don't exist in itself
+pack x = [if y == 0 then x!!y else if x!!y == x!!(y - 1) then (x !! (y - 1)) ++ x!!y else x !! y | y <- [0..length x-1]]
+-- ["a","a","a","a","b","c","c","a","a","d","e","e","e"]
+-- ["a","aa","aa","aa","b","c","cc","a","aa","d","e","ee","ee","*** Exception: Prelude.!!: index too large
+-- need to make a way to affect the previous one without adding on to list, I had length x without -1 so index too large
